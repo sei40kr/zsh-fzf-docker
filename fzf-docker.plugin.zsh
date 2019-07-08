5a1e7477a8a2e12d4977876e73b8b19cd1cb027e
Seong Yong-ju <sei40kr@gmail.com>
2019-07-08 23:18:00 +0900

    Initial commit

---
+++ fzf-docker.plugin.zsh
@@ -0,0 +1,16 @@
+# fzf-docker.plugin.zsh
+# author: Seong Yong-ju <sei40kr@gmail.com>
+
+fpath+="${0:h}/functions"
+
+autoload -Uz \
+         dcin \
+         dii \
+         dirm \
+         dni \
+         drm \
+         drmf \
+         dst \
+         dstp \
+         dvi \
+         dxb
